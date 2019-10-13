a136a7cc281b08f8b1d1501b857cc0831cf4ffc2
justvovka <vovkamokroff@gmail.com>
2019-10-13 17:14:42 +0300

    init

---
+++ src/components/App/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './App.scss';
+import Main from '../Main/';
+
+function App() {
+    return (
+        <div className="container">
+            <div className="row">
+                <Main/>
+            </div>
+        </div>
+    );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import App from './components/App/App';
+
+const getResource = async (url) => {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+        throw new Error(`Could not fetch ${url}`);
+    }
+    return await response.json();
+};
+
+getResource('http://localhost:8080/api/users/', { mode: 'no-cors'})
+    .then((body) => {
+        console.log(body);
+    })
+    .catch((err) => {
+        console.error('Could not fetch', err);
+    });
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
